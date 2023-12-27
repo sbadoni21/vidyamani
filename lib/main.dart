f4f0580600d19f5579fb1f27aee17b80af86fb14
Shivang <digital360india@gmail.com>
2023-12-27 15:41:34 +0530

    StreamBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:vidyamani/screens/home_page.dart';
+import 'package:vidyamani/screens/loginscreen.dart';
 import 'package:vidyamani/screens/splashscreen.dart';
+import 'package:vidyamani/services/auth/authentication.dart';
+
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,11 +30,72 @@ Future<void> main() async {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AuthenticationServices authServices = AuthenticationServices();
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  // }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: SplashScreen());
+    return MaterialApp(
+      title: 'Boarding Admissions',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      navigatorKey: navigatorKey,
+      // home: HomePage(),
+
+      home: StreamBuilder(
+        stream: authServices.firebaseAuth.authStateChanges(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const SplashScreen();
+          }
+
+          if (snapshot.hasError) {
+            print("Error with the stream: ${snapshot.error}");
+            return const Center(child: Text("An error occurred."));
+          }
+
+          if (snapshot.hasData && snapshot.data != null) {
+            print("User is authenticated. Navigating to HomePage.");
+            return HomePage();
+          }
+
+          print("User is not authenticated. Navigating to LoginPage.");
+
+          return LoginPage();
+        },
+      ),
+    );
+  }
+}
+
+class ErrorApp extends StatelessWidget {
+  final String errorMessage;
+
+  ErrorApp(this.errorMessage);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Error',
+      home: Scaffold(
+        body: Center(
+          child: Text(errorMessage),
+        ),
+      ),
+    );
   }
 }
