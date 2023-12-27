f44d32251f2014a902b736047dab8fbbd19c1554
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2023-12-27 11:41:27 +0530

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final logger = Logger(
+    printer: PrettyPrinter(),
+  );
+  try {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAA6conKiTEoYcpXEu5Q2Zh5QwZSxkPtWc",
+          authDomain: "notifymeapp-e884b.firebaseapp.com",
+          projectId: "notifymeapp-e884b",
+          storageBucket: "notifymeapp-e884b.appspot.com",
+          messagingSenderId: "681497384601",
+          appId: "1:681497384601:web:2270449befedf5f9abca08"),
+    );
+    logger.i("Firebase initialized successfully");
+    runApp(MyApp());
+  } catch (e) {
+    logger.e(e);
+  }
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
