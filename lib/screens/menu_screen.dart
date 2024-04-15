3cd9a61baa862b8d715f9e447df3224cb46301ab
sbadoni21 <97601162+sbadoni21@users.noreply.github.com>
2024-04-15 14:46:19 +0530

    changes to main menu

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -48,24 +48,74 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
         appBar: const CustomAppBarBckBtn(),
         body: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Column(
-                children: [
-                  Container(
-                    child: Center(
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: <Widget>[
-                          Container(
-                            width: double.infinity,
-                            height: 250,
-                            color: bgColor,
-                            child: const Center(),
-                          ),
-                          Positioned(
-                            left: 50,
-                            child: Container(
+            Column(
+              children: [
+                // Center(
+                //   child: Stack(
+                //     alignment: Alignment.center,
+                //     children: <Widget>[
+                //       Container(
+                //         width: double.infinity,
+                //         height: 250,
+                //         color: bgColor,
+                //         child: const Center(),
+                //       ),
+                //       Positioned(
+                //         left: 50,
+                //         child: Container(
+                //           width: 150,
+                //           height: 150,
+                //           decoration: const BoxDecoration(
+                //             shape: BoxShape.circle,
+                //             color: Colors.blue,
+                //           ),
+                //           child: CircleAvatar(
+                //             backgroundImage: (profilePhoto != null ||
+                //                     profilePhoto == 'none')
+                //                 ? NetworkImage(profilePhoto!) as ImageProvider
+                //                 : const AssetImage(
+                //                     'lib/assets/images/placeholder_image.png'),
+                //             radius: 20,
+                //           ),
+                //         ),
+                //       ),
+                //       Positioned(
+                //         left: 220,
+                //         child: Column(
+                //           mainAxisAlignment: MainAxisAlignment.start,
+                //           crossAxisAlignment: CrossAxisAlignment.start,
+                //           children: [
+                //             Text("Hi", style: myTextStylefontsize24),
+                //             SizedBox(
+                //               width: 100,
+                //               child: Text(
+                //                   displayName != null
+                //                       ? displayName as String
+                //                       : "Username",
+                //                   style: myTextStylefontsize24),
+                //             ),
+                //           ],
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
+
+                Container(
+                  color: bgColor,
+                  child: Center(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 40,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              width: 60,
+                            ),
+                            Container(
                               width: 150,
                               height: 150,
                               decoration: const BoxDecoration(
@@ -82,16 +132,16 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
                                 radius: 20,
                               ),
                             ),
-                          ),
-                          Positioned(
-                            left: 220,
-                            child: Column(
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
                                 Text("Hi", style: myTextStylefontsize24),
                                 SizedBox(
-                                  width: 150,
+                                  width: 100,
                                   child: Text(
                                       displayName != null
                                           ? displayName as String
@@ -100,86 +150,89 @@ class _MenuScreenState extends ConsumerState<MenuScreen> {
                                 ),
                               ],
                             ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Column(
-                    children: sideMenuItems.map((asset) {
-                      return SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            backgroundColor: Colors.white,
-                          ),
-                          onPressed: () async {
-                            if (asset['text'] == 'Logout') {
-                              ref
-                                  .read(userStateNotifierProvider.notifier)
-                                  .signOut();
-                              Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => LoginPage(),
-                                ),
-                              );
-                            } else {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => asset['route'],
-                                ),
-                              );
-                            }
-                          },
-                          child: Row(
-                            children: [
-                              SizedBox(
-                                width: 100,
-                                height: 70,
-                                child: Icon(
-                                  asset['icon'],
-                                  key: Key(
-                                    asset['key'],
-                                  ),
-                                  color: bgColor,
-                                ),
-                              ),
-                              Text(
-                                asset['text'],
-                                style: myTextStylefontsize16,
-                              ),
-                            ],
-                          ),
+                          ],
                         ),
-                      );
-                    }).toList(),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  const Padding(
-                    padding: const EdgeInsets.only(bottom: 16.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          "E-Learning Point Pvt. Ltd",
-                          style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                          ),
+                        SizedBox(
+                          height: 40,
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Column(
+                  children: sideMenuItems.map((asset) {
+                    return SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 0,
+                          backgroundColor: Colors.white,
+                        ),
+                        onPressed: () async {
+                          if (asset['text'] == 'Logout') {
+                            ref
+                                .read(userStateNotifierProvider.notifier)
+                                .signOut();
+                            Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LoginPage(),
+                              ),
+                            );
+                          } else {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => asset['route'],
+                              ),
+                            );
+                          }
+                        },
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 70,
+                              child: Icon(
+                                asset['icon'],
+                                key: Key(
+                                  asset['key'],
+                                ),
+                                color: bgColor,
+                              ),
+                            ),
+                            Text(
+                              asset['text'],
+                              style: myTextStylefontsize16,
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  }).toList(),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Padding(
+                  padding: const EdgeInsets.only(bottom: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "E-Learning Point Pvt. Ltd",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
